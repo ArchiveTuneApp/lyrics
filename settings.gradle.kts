29a80e399666a401878a0f2aaa39bb3c7454ad35
Hà Tiến Sáng <bemimnitran@gmail.com>
2026-06-22 07:11:37 +0700

    Initial import: 6 lyrics provider modules
    - kugou: KuGou lyrics API client
    - lrclib: LRC Lib API client
    - simpmusic: SimpMusic lyrics client
    - paxsenix: Multi-backend lyrics proxy client (Apple Music, NetEase, Spotify, Musixmatch, YouTube)
    - betterlyrics: BetterLyrics API client with TTML parser
    - unison: Unison lyrics client

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "lyrics"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+include(":kugou")
+include(":lrclib")
+include(":simpmusic")
+include(":paxsenix")
+include(":betterlyrics")
+include(":unison")
